37ef652fc7dda0a05f14479c0cd193f7efdac60a
김대희 <0824dh@naver.com>
2023-06-02 09:01:03 +0900

    feat: Arcive Post시 body에서 parm으로 변경

--- src/main/kotlin/com/example/easyptbackend/domain/presentation/ArchiveController.kt
+++ src/main/kotlin/com/example/easyptbackend/domain/presentation/ArchiveController.kt
@@ -2,23 +2,23 @@ package com.example.easyptbackend.domain.presentation
 
 import com.example.easyptbackend.domain.application.ArchiveService
 import com.example.easyptbackend.domain.presentation.dto.FindAllArchiveResponse
-import com.example.easyptbackend.domain.presentation.dto.RegisterArchiveRequest
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
-import javax.validation.Valid
 
+@RequestMapping("/archive")
 @RestController
 class ArchiveController(
     private val archiveService: ArchiveService
 ) {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun registerArchive(@RequestBody @Valid registerArchiveRequest: RegisterArchiveRequest) {
-        archiveService.registerArchive(registerArchiveRequest)
+    fun registerArchive(@RequestParam("user")user: String, @RequestParam("gpt")gpt: String) {
+        archiveService.registerArchive(user, gpt)
     }
 
     @GetMapping
